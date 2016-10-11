b4af2fe76d7a4b51fbbae51bd198b8c17684f7e9
tangege <342875368@qq.com>
2016-10-11 11:37:49 +0800

    添加ajax功能

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -3,12 +3,15 @@
  */
 function ajax (opts) {
     var xmlhttp;
+    var url;
+    var data;
     var settings = {
         url: "",
         type: "GET",
         async: true,
         cache: true,
-        data: null
+        data: "",
+        dataType: ""
     };
     var serialize = function (obj) {
         var result = [];
@@ -28,26 +31,68 @@ function ajax (opts) {
     }
     if(settings["url"] === ""){
         throw new Error("argument 'url' must be need!!!");
+    }else {
+        url = settings["url"];
+    }
+    if(settings["type"].toUpperCase() !== "GET" && settings["type"].toUpperCase() !== "POST"){
+        throw new Error("argument 'type' was wrong!!!");
     }
 
-    if(settings["type"].toUpperCase() === "GET"){
+    if(settings["type"].toUpperCase() !== "GET"){
 
+        if(settings["data"].constructor === Object){
+            data = serialize ( settings["data"] );
+            if(!settings["cache"]){
+                data += "&_" + (+new Date);
+            }
+        }else if(settings["data"].constructor === String){
+            data = settings["data"];
+            if(data && !settings["cache"]){
+                data += "&_" + (+new Date);
+            }else if(!data && !settings["cache"]){
+                data += "_" + (+new Date);
+            }
+        }else {
+            throw new Error("argument 'data' was wrong!!!");
+        }
+        url += "?" + data;
+    }else if(settings["type"].toUpperCase() !== "POST"){
+        data = settings["data"].constructor === Object?serialize ( settings["data"]):settings["data"];
+        if(!settings["cache"]){
+            url += "?_" + (+new Date);
+        }
+    }else {
+        throw new Error("argument 'type' was wrong!!!");
     }
 
-
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
-    }else {// code for IE6, IE5
+    }else {
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     xmlhttp.onreadystatechange = function(){
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
             if(settings["callback"]){
-                settings["callback"].call(this, xmlhttp.responseText);
+                var result = xmlhttp.responseText;
+                switch (settings["dataType"].toLowerCase()){
+                    case "json":
+                        result = JSON.parse(result);
+                        break;
+                    case "javascript":
+                        result = eval(result);
+                        break;
+                    case "xml":
+                        result = xmlhttp.responseXML;
+                        break;
+                }
+                settings["callback"].call(this, result);
             }
         }
     }
-    xmlhttp.open(settings["type"],"ajax_info.txt", settings["async"]);
-    xmlhttp.send();
-
+    xmlhttp.open(settings["type"], url, settings["async"]);
+    if(settings["type"].toUpperCase() === "POST"){
+        xmlhttp.send(data);
+    }else {
+        xmlhttp.send();
+    }
 }
